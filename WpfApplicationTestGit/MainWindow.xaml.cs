498c5efa0567b91d3f89ad66ce410de5d7ce4af8
ulanav <ulanav@rambler.ru>
2019-01-02 10:10:22 +0300

    change comments

--- WpfApplicationTestGit/MainWindow.xaml.cs
+++ WpfApplicationTestGit/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WpfApplicationTestGit
     {
         public MainWindow()
         {
-            InitializeComponent(); // comments
+            InitializeComponent(); //  comments chanched
         }
 
     }
