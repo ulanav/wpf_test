10972313dc0d3a6218155c2a58c8aff406e8a963
ulanav <ulanav@rambler.ru>
2019-01-02 10:14:27 +0300

    Secondary change comments

--- WpfApplicationTestGit/MainWindow.xaml.cs
+++ WpfApplicationTestGit/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WpfApplicationTestGit
     {
         public MainWindow()
         {
-            InitializeComponent(); //  comments chanched
+            InitializeComponent(); //  comments secondary chanched 
         }
 
     }
